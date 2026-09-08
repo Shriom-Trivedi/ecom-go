0087685ebe90616eeacd5d78964129bc5c75e187
Shri Om Trivedi <tshriom@gmail.com>
2026-09-08 10:40:24 +0530

    db conn added to application layer

--- cmd/api.go
+++ cmd/api.go
@@ -5,9 +5,11 @@ import (
 	"net/http"
 	"time"
 
+	repo "github.com/Shriom-Trivedi/ecom-go/internal/adapters/postgresql/sqlc"
 	"github.com/Shriom-Trivedi/ecom-go/internal/products"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5"
 )
 
 // mount
@@ -29,7 +31,7 @@ func (app *application) mount() http.Handler {
 		w.Write([]byte("The server is ACTIVE.")) // health check endpoint
 	})
 
-	productService := products.NewService()
+	productService := products.NewService(repo.New(app.db))
 	productHandler := products.NewHandler(productService)
 	r.Get("/products", productHandler.ListProducts)
 
@@ -55,7 +57,7 @@ func (app *application) run(h http.Handler) error {
 type application struct {
 	config config
 	// logger
-	// db driver
+	db *pgx.Conn
 }
 
 type config struct {
